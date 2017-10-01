8a261b33a15eb63e819b1eae7f3d741b163846c6
charliecharles <charlie@kiwicampus.com>
2017-09-30 17:27:06 -0700

    init

---
+++ esp8266/sonoffsc/comms.ino
@@ -0,0 +1,145 @@
+/*
+
+COMMUNICATIONS MODULE
+
+Copyright (C) 2016 by Xose Pérez <xose dot perez at gmail dot com>
+
+*/
+
+#include "SerialLink.h"
+SerialLink link(Serial, false);
+
+const PROGMEM char at_push[] = "AT+PUSH";
+const PROGMEM char at_every[] = "AT+EVERY";
+const PROGMEM char at_temp[] = "AT+TEMP";
+const PROGMEM char at_hum[] = "AT+HUM";
+const PROGMEM char at_dust[] = "AT+DUST";
+const PROGMEM char at_noise[] = "AT+NOISE";
+const PROGMEM char at_light[] = "AT+LIGHT";
+const PROGMEM char at_clap[] = "AT+CLAP";
+const PROGMEM char at_code[] = "AT+CODE";
+const PROGMEM char at_thld[] = "AT+THLD";
+const PROGMEM char at_r[] = "AT+R";
+const PROGMEM char at_g[] = "AT+G";
+const PROGMEM char at_b[] = "AT+B";
+const PROGMEM char at_effect[] = "AT+EFFECT";
+const PROGMEM char at_rgb_exec[] = "AT+RGBEXEC";
+
+// -----------------------------------------------------------------------------
+// VALUES
+// -----------------------------------------------------------------------------
+
+float temperature;
+int humidity;
+int light;
+float dust;
+int noise;
+uint32 rgb;
+
+float getTemperature() { return temperature; }
+float getHumidity() { return humidity; }
+float getLight() { return light; }
+float getDust() { return dust; }
+float getNoise() { return noise; }
+uint32 getRGB() { return rgb; }
+
+// -----------------------------------------------------------------------------
+// COMMUNICATIONS
+// -----------------------------------------------------------------------------
+
+bool commsGet(char * key) {
+    return false;
+}
+
+bool commsSet(char * key, int value) {
+
+    char buffer[50];
+    DEBUG_MSG("Value is %d", value);
+
+    if (strcmp_P(key, at_code) == 0) {
+        mqttSend(getSetting("mqttTopicClap", MQTT_CLAP_TOPIC).c_str(), value);
+        return true;
+    }
+
+    if (strcmp_P(key, at_temp) == 0) {
+        temperature = (float) value / 10;
+        mqttSend(getSetting("mqttTopicTemp", MQTT_TEMPERATURE_TOPIC).c_str(), temperature);
+        domoticzSend("dczIdxTemp", temperature);
+        sprintf(buffer, "{\"sensorTemp\": %s}", String(temperature).c_str());
+        wsSend(buffer);
+        return true;
+    }
+
+    if (strcmp_P(key, at_hum) == 0) {
+        humidity = value;
+        mqttSend(getSetting("mqttTopicHum", MQTT_HUMIDITY_TOPIC).c_str(), humidity);
+        domoticzSend("dczIdxHum", humidity);
+        sprintf(buffer, "{\"sensorHum\": %d}", humidity);
+        wsSend(buffer);
+        return true;
+    }
+
+    if (strcmp_P(key, at_light) == 0) {
+        light = value;
+        mqttSend(getSetting("mqttTopicLight", MQTT_LIGHT_TOPIC).c_str(), light);
+        domoticzSend("dczIdxLight", light);
+        sprintf(buffer, "{\"sensorLight\": %d}", light);
+        wsSend(buffer);
+        return true;
+    }
+
+    if (strcmp_P(key, at_dust) == 0) {
+        dust = (float) value / 100;
+        mqttSend(getSetting("mqttTopicDust", MQTT_DUST_TOPIC).c_str(), dust);
+        domoticzSend("dczIdxDust", dust);
+        sprintf(buffer, "{\"sensorDust\": %s}", String(dust).c_str());
+        wsSend(buffer);
+        return true;
+    }
+
+    if (strcmp_P(key, at_noise) == 0) {
+        noise = value;
+        mqttSend(getSetting("mqttTopicNoise", MQTT_NOISE_TOPIC).c_str(), noise);
+        domoticzSend("dczIdxNoise", noise);
+        sprintf(buffer, "{\"sensorNoise\": %d}", noise);
+        wsSend(buffer);
+        return true;
+    }
+
+    return false;
+
+}
+
+void commConfigure() {
+    link.send_P(at_every, getSetting("sensorEvery", SENSOR_EVERY).toInt());
+    link.send_P(at_clap, getSetting("clapEnabled", SENSOR_CLAP_ENABLED).toInt() == 1 ? 1 : 0);
+    link.send_P(at_push, 1);
+}
+
+void commsSetup() {
+
+    link.onGet(commsGet);
+    link.onSet(commsSet);
+
+    static WiFiEventHandler e3 = WiFi.onStationModeConnected([](const WiFiEventStationModeConnected& event) {
+        commConfigure();
+    });
+
+    static WiFiEventHandler e4 = WiFi.onSoftAPModeStationConnected([](const WiFiEventSoftAPModeStationConnected& event) {
+        commConfigure();
+    });
+
+}
+
+void commsLoop() {
+    link.handle();
+}
+
+// Function to send AT commands that set rgb colors.
+void commSendRGB(int r, int g, int b) {
+
+	link.send_P(at_r, r);			// Send red value
+	link.send_P(at_g, g);			// Send blue value
+	link.send_P(at_b, b);			// Send green value
+	link.send_P(at_rgb_exec, 1);	// Send execute instruction
+}
---
+++ esp8266/sonoffsc/sonoffsc.ino
@@ -0,0 +1,247 @@
+/*
+
+Sonoff SC
+Copyright (C) 2017 by Xose Pérez <xose dot perez at gmail dot com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <Arduino.h>
+#include "config/all.h"
+
+// -----------------------------------------------------------------------------
+// PROTOTYPES
+// -----------------------------------------------------------------------------
+
+#include <ESPAsyncWebServer.h>
+#include <AsyncMqttClient.h>
+#include <ArduinoJson.h>
+
+
+void mqttRegister(void (*callback)(unsigned int, const char *, const char *));
+template<typename T> bool setSetting(const String& key, T value);
+template<typename T> String getSetting(const String& key, T defaultValue);
+template<typename T> void mqttSend(const char * topic, T message);
+template<typename T> void domoticzSend(unsigned int idx, T value);
+template<typename T> void domoticzSend(const char * setting, T value);
+
+// -----------------------------------------------------------------------------
+// METHODS
+// -----------------------------------------------------------------------------
+
+String getIdentifier() {
+    char identifier[20];
+    sprintf(identifier, "%s_%06X", DEVICE, ESP.getChipId());
+    return String(identifier);
+}
+
+bool ledStatus(bool status) {
+    digitalWrite(LED_PIN, status ? LOW : HIGH);
+    return status;
+}
+
+bool ledStatus() {
+    return (digitalRead(LED_PIN) == LOW);
+}
+
+bool ledToggle() {
+    return ledStatus(!ledStatus());
+}
+
+void ledBlink(unsigned long delayOff, unsigned long delayOn) {
+    static unsigned long next = millis();
+    if (next < millis()) {
+        next += (ledToggle() ? delayOn : delayOff);
+    }
+}
+
+void showStatus() {
+    if (wifiConnected()) {
+        if (WiFi.getMode() == WIFI_AP) {
+            ledBlink(2000, 2000);
+        } else {
+            ledBlink(5000, 500);
+        }
+    } else {
+        ledBlink(500, 500);
+    }
+}
+
+void hardwareSetup() {
+    Serial.begin(SERIAL_BAUDRATE);
+    pinMode(LED_PIN, OUTPUT);
+    SPIFFS.begin();
+}
+
+void hardwareLoop() {
+
+    showStatus();
+
+    // Heartbeat
+    static unsigned long last_heartbeat = 0;
+    if (mqttConnected()) {
+        if ((millis() - last_heartbeat > HEARTBEAT_INTERVAL) || (last_heartbeat == 0)) {
+            last_heartbeat = millis();
+            mqttSend(MQTT_HEARTBEAT_TOPIC, "1");
+            DEBUG_MSG("[BEAT] Free heap: %d\n", ESP.getFreeHeap());
+        }
+    }
+
+}
+
+// -----------------------------------------------------------------------------
+// BOOTING
+// -----------------------------------------------------------------------------
+
+void welcome() {
+
+    delay(2000);
+    DEBUG_MSG("%s %s\n", (char *) APP_NAME, (char *) APP_VERSION);
+    DEBUG_MSG("%s\n%s\n\n", (char *) APP_AUTHOR, (char *) APP_WEBSITE);
+    //DEBUG_MSG("Device: %s\n", (char *) getIdentifier().c_str());
+    DEBUG_MSG("ChipID: %06X\n", ESP.getChipId());
+    DEBUG_MSG("CPU frequency: %d MHz\n", ESP.getCpuFreqMHz());
+    DEBUG_MSG("Last reset reason: %s\n", (char *) ESP.getResetReason().c_str());
+    DEBUG_MSG("Memory size: %d bytes\n", ESP.getFlashChipSize());
+    DEBUG_MSG("Free heap: %d bytes\n", ESP.getFreeHeap());
+    DEBUG_MSG("Firmware size: %d bytes\n", ESP.getSketchSize());
+    DEBUG_MSG("Free firmware space: %d bytes\n", ESP.getFreeSketchSpace());
+    FSInfo fs_info;
+    if (SPIFFS.info(fs_info)) {
+        DEBUG_MSG("File system total size: %d bytes\n", fs_info.totalBytes);
+        DEBUG_MSG("            used size : %d bytes\n", fs_info.usedBytes);
+        DEBUG_MSG("            block size: %d bytes\n", fs_info.blockSize);
+        DEBUG_MSG("            page size : %d bytes\n", fs_info.pageSize);
+        DEBUG_MSG("            max files : %d\n", fs_info.maxOpenFiles);
+        DEBUG_MSG("            max length: %d\n", fs_info.maxPathLength);
+    }
+    DEBUG_MSG("\n\n");
+
+}
+
+void setup() {
+
+    Serial.begin(19200);
+
+    hardwareSetup();
+    welcome();
+
+    settingsSetup();
+    if (getSetting("hostname").length() == 0) {
+        setSetting("hostname", getIdentifier());
+        saveSettings();
+    }
+    DEBUG_MSG("buttonSetup()");
+    buttonSetup();
+    DEBUG_MSG("wifiSetup()");
+    wifiSetup();
+    DEBUG_MSG("otaSetup()");
+    otaSetup();
+    DEBUG_MSG("mqttSetup()");
+    mqttSetup();
+    DEBUG_MSG("webSetup()");
+    webSetup();
+    DEBUG_MSG("commsSetup()");
+    //commsSetup();
+    DEBUG_MSG("fauxmoSetup()");
+    fauxmoSetup();
+    DEBUG_MSG("commConfigure()");
+	  commConfigure();
+
+
+
+	mqttRegister(mqttCallback);
+}
+
+void loop() {
+    //DEBUG_MSG("Main() Loop %d", getTemperature());
+    hardwareLoop();
+    //commsLoop();
+
+    readUart();
+
+    buttonLoop();
+    wifiLoop();
+    otaLoop();
+    mqttLoop();
+    fauxmoLoop();
+
+}
+
+
+
+
+void readUart(void)
+{
+    static bool get_at_flag = false;
+    static String rec_string = "";
+    int16_t index1;
+
+    while ((Serial.available() > 0))
+    {
+        char a = (char)Serial.read();
+        rec_string +=a;
+
+        if(a == 0x1B)
+        {
+             break;
+        }
+    }
+
+    StaticJsonBuffer<200> jsonBuffer;
+
+    //DEBUG_MSG("Read: %s", rec_string.c_str());
+    if(-1 != rec_string.indexOf(0x1B))
+    {
+
+      DEBUG_MSG("Read: %s", rec_string.c_str());
+        if(-1 != (index1 = rec_string.indexOf("AT+STATUS?")))
+        {
+          Serial.write("AT+STATUS=4");
+        DEBUG_MSG("Send status=4");
+        }
+        else if (-1 != (index1 = rec_string.indexOf("AT+UPDATE")))
+        {
+          Serial.write("AT+SEND=ok");
+          String msg = "{"+rec_string.substring(10,rec_string.length()-1)+"}";
+          JsonObject& root = jsonBuffer.parseObject(msg.c_str());
+          int humidity = root["humidity"];
+          double temp  = 1.8*double(root["temperature"])+32;
+          int noise    = root["noise"];
+          int dust   = root["dusty"];
+          int light   = root["light"];
+          DEBUG_MSG("UPDATE! %s", msg.c_str());
+          DEBUG_MSG("Data: %d %d %d %d %d", humidity, temp, noise, dust, light);
+          mqttSend(getSetting("mqttTopicTemp", MQTT_TEMPERATURE_TOPIC).c_str(), temp);
+          mqttSend(getSetting("mqttTopicHum", MQTT_HUMIDITY_TOPIC).c_str(), humidity);
+          mqttSend(getSetting("mqttTopicLight", MQTT_LIGHT_TOPIC).c_str(), light);
+          mqttSend(getSetting("mqttTopicDust", MQTT_DUST_TOPIC).c_str(), dust);
+          mqttSend(getSetting("mqttTopicNoise", MQTT_NOISE_TOPIC).c_str(), noise);
+        }
+        else
+        {
+            /*do nothing*/
+        }
+        rec_string = "";
+    }
+    else if(-1 == rec_string.indexOf("AT"))
+    {
+        Serial.flush();
+    }
+    else
+    {
+        /*do nothing*/
+    }
+}
