be53d4ebf2e40012722974822c7b070a079b54e7
charliecharles <charlie@kiwicampus.com>
2017-10-10 11:19:16 -0700

    new with aws sdk update shadow

---
+++ esp8266/src/main.ino
@@ -0,0 +1,216 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <AmazonIOTClient.h>
+#include <ESP8266AWSImplementations.h>
+
+#include <EEPROM.h> //library to handle with permament data after turning off Device
+#include "ESP8266TrueRandom.h"
+
+#include <ArduinoJson.h>
+
+Esp8266HttpClient httpClient;
+Esp8266DateTimeProvider dateTimeProvider;
+
+AmazonIOTClient iotClient;
+ActionError actionError;
+
+const char* ssid = "Andres7";
+const char* password = "99999999";
+char shadow[800];
+String clientID;
+byte uuidNumber[16];
+
+String chip_id;
+
+String ID_DELIMETER = "node-";
+
+int UUIDLEN = 36 + ID_DELIMETER.length();
+
+
+String read_eeprom(int index, int len){
+  String str = "";
+  for (int i = index; i < len + index; ++i)
+    str.concat( char(EEPROM.read(i)) );
+  return str;
+}
+
+
+void write_eeprom(uint32_t address, String str){
+  for (int i = 0; i < str.length(); i++)
+      EEPROM.write(address+i, str.charAt(i));
+  EEPROM.commit();
+}
+
+String get_clientId(){
+  Serial.print("Getting ID: ");
+  String str = read_eeprom(0,UUIDLEN);
+  Serial.println(str);
+  if (str.substring(0,ID_DELIMETER.length()) == ID_DELIMETER)
+    Serial.println("Already ID");
+  else{
+    Serial.println("need to generate ID");
+    ESP8266TrueRandom.uuid(uuidNumber);
+    str = ID_DELIMETER + ESP8266TrueRandom.uuidToString(uuidNumber);
+    Serial.println("ID: " + str);
+    write_eeprom(0,str);
+    Serial.println("ID (in memory): " +read_eeprom(0,UUIDLEN));
+  }
+  return str;
+}
+
+
+void readUart(void)
+{
+    static bool get_at_flag = false;
+    static String rec_string = "";
+    int16_t index1;
+
+    while ((Serial.available() > 0))
+    {
+        char a = (char)Serial.read();
+        rec_string +=a;
+
+        if(a == 0x1B)
+        {
+             break;
+        }
+    }
+
+    StaticJsonBuffer<200> jsonBuffer;
+    //DEBUG_MSG("Read: %s", rec_string.c_str());
+    Serial.println(rec_string);
+    if(-1 != rec_string.indexOf(0x1B))
+    {
+      delay(100);
+      // DEBUG_MSG("Read: %s", rec_string.c_str());
+        if(-1 != (index1 = rec_string.indexOf("AT+STATUS?")))
+        {
+          Serial.write("AT+STATUS=4");
+        // DEBUG_MSG("Send status=4");
+        }
+        else if (-1 != (index1 = rec_string.indexOf("AT+UPDATE")))
+        {
+          Serial.write("AT+SEND=ok");
+          String msg = "{"+rec_string.substring(10,rec_string.length()-1)+"}";
+          JsonObject& root = jsonBuffer.parseObject(msg.c_str());
+          int humidity = root["humidity"];
+          double temp  = 1.8*double(root["temperature"])+32;
+          int noise    = root["noise"];
+          int dust   = root["dusty"];
+          int light   = root["light"];
+
+          Serial.println("UPDATE! " + msg);
+          // DEBUG_MSG("Data: %d %d %d %d %d", humidity, temp, noise, dust, light);
+
+          String config_msg =  "\"config\": {\"bemo_type\": \"bemo-test\", \"version\": \"0.0.1\", \"iotname\": \"esp8266_"+chip_id+"\" }";
+
+          String light_msg = "\"light_sensor\": {\"light_value\":" + String(light) + ", \"light_value_old\":" + String(light) + ", \"antiglare\": 0, \"reliability\": 1}";
+          String temp_msg = "\"temperature_sensor\": {\"temperature_value\":" + String(temp) + ", \"temperature_value_old\":" + String(temp) + ", \"reliability\": 1}";
+          String humidity_msg = "\"humidity_sensor\": {\"humidity_value\":" + String(humidity) + ", \"humidity_value_old\":" + String(humidity) + ", \"reliability\": 1}";
+          String noise_msg = "\"noise_sensor\": {\"noise_value\":" + String(noise) + ", \"noise_value_old\":" + String(noise) + ", \"reliability\": 1}";
+          String dust_msg = "\"dust_sensor\": {\"dust_value\":" + String(dust) + ", \"dust_value_old\":" + String(dust) + ", \"reliability\": 1}";
+
+          String total_msg = config_msg + "," + light_msg + "," + temp_msg + "," + humidity_msg + "," + noise_msg + "," + dust_msg;
+
+          long mem = ESP.getFreeHeap();
+
+          // msg = "{\"state\":{\"reported\":" + msg + "}}";
+          msg = "{\"state\":{\"reported\":{" + total_msg + "} }}";
+
+          strcpy(shadow, msg.c_str());
+          //strcpy(shadow, ("{\"state\":{\"reported\":{\"test_value1\":6, \"test_value2\":1}} }"));
+          char* result = iotClient.update_shadow(shadow, actionError);
+          // Serial.print(result);
+          delay(6000);
+          ESP.getFreeHeap();
+          delete[] result;
+          // delay(5000);
+          // DEBUG_MSG("Message: %s", result);
+
+        }
+        else
+        {
+            /*do nothing*/
+        }
+        rec_string = "";
+    }
+    else if(-1 == rec_string.indexOf("AT"))
+    {
+        Serial.flush();
+    }
+    else
+    {
+        /*do nothing*/
+    }
+}
+
+
+
+void initWLAN()
+{
+  WiFi.begin(ssid, password);
+  Serial.print("Trying to connect to wifi ");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(". ");
+  }
+  Serial.println("Connected! ");
+}
+
+void initAWS()
+{
+  iotClient.setAWSRegion("us-east-2");
+  iotClient.setAWSEndpoint("amazonaws.com");
+  iotClient.setAWSDomain("a1ud2nqx6i62qf.iot.us-east-2.amazonaws.com");
+  iotClient.setAWSPath("/things/iotBemo/shadow");
+  iotClient.setAWSKeyID("AKIAISLB53XWR6LH266Q");
+  iotClient.setAWSSecretKey("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");
+  iotClient.setHttpClient(&httpClient);
+  iotClient.setDateTimeProvider(&dateTimeProvider);
+}
+
+void setup() {
+  Serial.begin(19200);
+  delay(10);
+  Serial.println("begin");
+  initWLAN();
+  Serial.println("wlan initialized");
+  initAWS();
+  Serial.println("iot initialized");
+
+  // clientID = get_clientId();
+  long ID = ESP.getChipId();
+  chip_id = String(ID, HEX);
+  Serial.println(chip_id.c_str());
+
+}
+
+void loop()
+{
+
+  long mem = ESP.getFreeHeap();
+  // Serial.print("freeMemory()=");
+  //   Serial.println(mem);
+
+
+  // // strcpy(shadow, ("{\"state\":{\"reported\":{\"test_value1\":66, \"test_value2\":666}}, \"id_\": \""+ clientID + "\" }").c_str());
+  // strcpy(shadow, ("{\"state\":{\"reported\":{\"humidity\":16, \"light\":10, \"temperature\":25, \"noise\":1, \"dusty\":1}} }"));
+  //
+  // Serial.println("Trying to send data");
+  // Serial.print(shadow);
+  //
+  // char* result = iotClient.update_shadow(shadow, actionError);
+  // Serial.print(result);
+  //
+  // delete[] result;
+
+  readUart();
+
+
+  // Serial.printf ("ESP8266 Chip id =%08X \n", ESP.getChipId ());
+  // Serial.printf ("ESP8266 Chip id =%s \n", id.c_str());
+
+  delay(1000);
+}
