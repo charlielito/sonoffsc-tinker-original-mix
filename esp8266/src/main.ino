84be6e6816d93b49624516c24c17ba073e88bbb7
charliecharles <charlie@kiwicampus.com>
2017-10-19 00:02:23 -0700

    fixed memory leaks

--- esp8266/src/main.ino
+++ esp8266/src/main.ino
@@ -1,34 +1,74 @@
 #include <Arduino.h>
+#include <Stream.h>
 
 #include <ESP8266WiFi.h>
-#include <AmazonIOTClient.h>
-#include <ESP8266AWSImplementations.h>
+#include <ESP8266WiFiMulti.h>
+
+//AWS
+#include "sha256.h"
+#include "Utils.h"
+
+//WEBSockets
+#include <Hash.h>
+#include <WebSocketsClient.h>
+
+//MQTT PAHO
+#include <SPI.h>
+#include <IPStack.h>
+#include <Countdown.h>
+#include <MQTTClient.h>
+
+
+//AWS MQTT Websocket
+#include "Client.h"
+#include "AWSWebSocketClient.h"
+#include "CircularByteBuffer.h"
+
+
 
 #include <EEPROM.h> //library to handle with permament data after turning off Device
 
 #include <ArduinoJson.h>
 
-Esp8266HttpClient httpClient;
-Esp8266DateTimeProvider dateTimeProvider;
-
-AmazonIOTClient iotClient;
-ActionError actionError;
-
-// Your WIFI Setttings
-const char* ssid = "Time_machine";
-const char* password = "P66988165r";
-
-// Your IAM credentials
-const char* AWSKeyID = "AKIAISLB53XWR6LH266Q";
-const char* AWSSecretKey = "N12kNqd9QFX0AqopLlJEogHXiz+17o5Eqm2UfV7p";
-
-const char* AWSPath = "/things/iotBemo/shadow";
-const char* AWSDomain = "a1ud2nqx6i62qf.iot.us-east-2.amazonaws.com";
-const char* AWSRegion = "us-east-2";
 
 
-char shadow[800];
+//  --------- Config ---------- //
+//AWS IOT config, change these:
+char wifi_ssid[]       = "kiwibot0";
+char wifi_password[]   = "kiwicampus0";
+char aws_endpoint[]    = "a1ud2nqx6i62qf.iot.us-east-2.amazonaws.com";
+char aws_key[]         = "AKIAISLB53XWR6LH266Q";
+char aws_secret[]      = "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";
+char aws_region[]      = "us-east-2";
+const char* aws_topic  = "$aws/things/iotBemo/shadow/update";
+int port = 443;
 
+//MQTT config
+const int maxMQTTpackageSize = 1024;
+const int maxMQTTMessageHandlers = 1;
+
+ESP8266WiFiMulti WiFiMulti;
+
+AWSWebSocketClient awsWSclient(1000);
+
+IPStack ipstack(awsWSclient);
+MQTT::Client<IPStack, Countdown, maxMQTTpackageSize, maxMQTTMessageHandlers> *client = NULL;
+
+//# of connections
+long connection = 0;
+
+//generate random mqtt clientID
+char* generateClientID () {
+  char* cID = new char[23]();
+  for (int i=0; i<22; i+=1)
+    cID[i]=(char)random(1, 256);
+  return cID;
+}
+
+//count messages arrived
+int arrivedcount = 0;
+
+char shadow[700];
 
 String chip_id;
 
@@ -108,12 +148,20 @@ void readUart(void)
           msg = "{\"state\":{\"reported\":{" + total_msg + "} }}";
 
           strcpy(shadow, msg.c_str());
-          //strcpy(shadow, ("{\"state\":{\"reported\":{\"test_value1\":6, \"test_value2\":1}} }"));
-          char* result = iotClient.update_shadow(shadow, actionError);
-          // Serial.print(result);
-          delay(6000);
-          ESP.getFreeHeap();
-          delete[] result;
+
+
+
+          //send a message
+          MQTT::Message message;
+          // char buf[100];
+          // strcpy(buf, "{\"state\":{\"reported\":{\"on\": false}, \"desired\":{\"on\": false}}}");
+          message.qos = MQTT::QOS0;
+          message.retained = false;
+          message.dup = false;
+          message.payload = (void*)shadow;
+          message.payloadlen = strlen(shadow)+1;
+          int rc = client->publish(aws_topic, message);
+
           // delay(5000);
           // DEBUG_MSG("Message: %s", result);
 
@@ -136,57 +184,154 @@ void readUart(void)
 
 
 
-void initWLAN()
+
+//callback to handle mqtt messages
+void messageArrived(MQTT::MessageData& md)
 {
-  WiFi.begin(ssid, password);
-  Serial.print("Trying to connect to wifi ");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(". ");
-  }
-  Serial.println("Connected! ");
+  MQTT::Message &message = md.message;
+
+  Serial.print("Message ");
+  Serial.print(++arrivedcount);
+  Serial.print(" arrived: qos ");
+  Serial.print(message.qos);
+  Serial.print(", retained ");
+  Serial.print(message.retained);
+  Serial.print(", dup ");
+  Serial.print(message.dup);
+  Serial.print(", packetid ");
+  Serial.println(message.id);
+  Serial.print("Payload ");
+  char* msg = new char[message.payloadlen+1]();
+  memcpy (msg,message.payload,message.payloadlen);
+  Serial.println(msg);
+  delete msg;
 }
 
-void initAWS()
-{
-  iotClient.setAWSRegion(AWSRegion);
-  iotClient.setAWSEndpoint("amazonaws.com");
-  iotClient.setAWSDomain(AWSDomain);
-  iotClient.setAWSPath(AWSPath );
-  iotClient.setAWSKeyID(AWSKeyID);
-  iotClient.setAWSSecretKey(AWSSecretKey);
-  iotClient.setHttpClient(&httpClient);
-  iotClient.setDateTimeProvider(&dateTimeProvider);
+//connects to websocket layer and mqtt layer
+bool connect () {
+
+    if (client == NULL) {
+      client = new MQTT::Client<IPStack, Countdown, maxMQTTpackageSize, maxMQTTMessageHandlers>(ipstack);
+    } else {
+
+      if (client->isConnected ()) {
+        client->disconnect ();
+      }
+      delete client;
+      client = new MQTT::Client<IPStack, Countdown, maxMQTTpackageSize, maxMQTTMessageHandlers>(ipstack);
+    }
+
+
+    //delay is not necessary... it just help us to get a "trustful" heap space value
+    delay (1000);
+    Serial.print (millis ());
+    Serial.print (" - conn: ");
+    Serial.print (++connection);
+    Serial.print (" - (");
+    Serial.print (ESP.getFreeHeap ());
+    Serial.println (")");
+
+
+
+
+   int rc = ipstack.connect(aws_endpoint, port);
+    if (rc != 1)
+    {
+      Serial.println("error connection to the websocket server");
+      return false;
+    } else {
+      Serial.println("websocket layer connected");
+    }
+
+
+    Serial.println("MQTT connecting");
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.MQTTVersion = 3;
+    char* clientID = generateClientID ();
+    data.clientID.cstring = clientID;
+    rc = client->connect(data);
+    delete[] clientID;
+    if (rc != 0)
+    {
+      Serial.print("error connection to MQTT server");
+      Serial.println(rc);
+      return false;
+    }
+    Serial.println("MQTT connected");
+    return true;
 }
+
+//subscribe to a mqtt topic
+void subscribe () {
+   //subscript to a topic
+    int rc = client->subscribe(aws_topic, MQTT::QOS0, messageArrived);
+    if (rc != 0) {
+      Serial.print("rc from MQTT subscribe is ");
+      Serial.println(rc);
+      return;
+    }
+    Serial.println("MQTT subscribed");
+}
+
+//send a message to a mqtt topic
+void sendmessage () {
+    //send a message
+    MQTT::Message message;
+    char buf[100];
+    strcpy(buf, "{\"state\":{\"reported\":{\"on\": false}, \"desired\":{\"on\": false}}}");
+    message.qos = MQTT::QOS0;
+    message.retained = false;
+    message.dup = false;
+    message.payload = (void*)buf;
+    message.payloadlen = strlen(buf)+1;
+    int rc = client->publish(aws_topic, message);
+}
+
 
 void setup() {
-  Serial.begin(19200);
-  delay(10);
-  Serial.println("begin");
-  initWLAN();
-  Serial.println("wlan initialized");
-  initAWS();
-  Serial.println("iot initialized");
+    Serial.begin (19200);
+    delay (2000);
+    Serial.setDebugOutput(1);
 
-  // clientID = get_clientId();
-  long ID = ESP.getChipId();
-  chip_id = String(ID, HEX);
-  Serial.println(chip_id.c_str());
+    //fill with ssid and wifi password
+    WiFiMulti.addAP(wifi_ssid, wifi_password);
+    Serial.println ("connecting to wifi");
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        delay(100);
+        Serial.print (".");
+    }
+    Serial.println ("\nconnected");
+
+    configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+
+    //fill AWS parameters
+    awsWSclient.setAWSRegion(aws_region);
+    awsWSclient.setAWSDomain(aws_endpoint);
+    awsWSclient.setAWSKeyID(aws_key);
+    awsWSclient.setAWSSecretKey(aws_secret);
+    awsWSclient.setUseSSL(true);
+
+    if (connect ()){
+      subscribe ();
+      sendmessage ();
+    }
 
 }
 
-void loop()
-{
+void loop() {
+  //keep the mqtt up and running
+  if (awsWSclient.connected ()) {
+      client->yield();
+      // sendmessage();
+      readUart();
+  } else {
+    //handle reconnection
+    if (connect ()){
+      subscribe ();
+    }
+  }
 
-  long mem = ESP.getFreeHeap();
-  // Serial.print("freeMemory()=");
-  //   Serial.println(mem);
+  delay(5000);
 
-  readUart();
-
-  // Serial.printf ("ESP8266 Chip id =%08X \n", ESP.getChipId ());
-  // Serial.printf ("ESP8266 Chip id =%s \n", id.c_str());
-
-  delay(1000);
 }
